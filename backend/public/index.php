935b0f327349eda244f6f7123c7a548bc308bbbb
Baptiste Delaborde <delabordebaptiste8@gmail.com>
2025-10-20 15:40:07 +0200

    probleme CORS

--- backend/public/index.php
+++ backend/public/index.php
@@ -58,5 +58,13 @@ $app->get('/{path:.*}', function ($request, $response, $args) {
     return $response;
 });
 
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 // === Run ===
 $app->run();
