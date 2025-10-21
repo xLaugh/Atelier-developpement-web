b8f3ee5a166610e06f463460a6930157814fc54c
Laugh <70723697+xLaugh@users.noreply.github.com>
2025-10-21 09:41:06 +0200

    Suppression username par prénom nom

--- sql/bdd.sql
+++ sql/bdd.sql
@@ -32,7 +32,8 @@ CREATE TABLE items (
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+    prenom VARCHAR(50) NOT NULL UNIQUE,
+    nom VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role ENUM('user', 'admin') DEFAULT 'user',
