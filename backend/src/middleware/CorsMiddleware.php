b41646ea0ea23d598a90c903f28eaed155f96fed
Baptiste Delaborde <delabordebaptiste8@gmail.com>
2025-10-24 09:46:18 +0200

    probleme cors

--- backend/src/middleware/CorsMiddleware.php
+++ backend/src/middleware/CorsMiddleware.php
@@ -6,18 +6,19 @@ namespace App\middleware;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Slim\Psr7\Response as SlimResponse;
 
 class CorsMiddleware
 {
     public function __invoke(Request $request, RequestHandler $handler): Response
     {
-        // Gérer les requêtes OPTIONS (preflight) sans ajouter d'en-têtes (gérés par Apache)
-        if ($request->getMethod() === 'OPTIONS') {
-            return new SlimResponse(204);
-        }
+        $response = $request->getMethod() === 'OPTIONS'
+            ? (new \Slim\Psr7\Response())->withStatus(204)
+            : $handler->handle($request);
 
-        // Pass-through; les en-têtes CORS sont appliqués par Apache (.htaccess)
-        return $handler->handle($request);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://docketu.iutnc.univ-lorraine.fr:13014')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Credentials', 'true');
     }
 }
